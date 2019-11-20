160217da48a229352fce89bd826f6accc3915c9f
Richard Wood <richardwood5@hotmail.com>
2019-11-20 23:35:32 +0000

    feat: added bubble graph

--- static/js/main.js
+++ static/js/main.js
@@ -64,3 +64,124 @@ function changeDates(values) {
     durationBar.wrangleData();
     stackedArea.wrangleData();
 }
+
+// var margin = { left:80, right:20, top:50, bottom:100 };
+// var height = 500 - margin.top - margin.bottom, 
+//     width = 800 - margin.left - margin.right;
+
+// var g = d3.select("#bubble-chart")
+//     .append("svg")
+//         .attr("width", width + margin.left + margin.right)
+//         .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//         .attr("transform", "translate(" + margin.left + 
+//             ", " + margin.top + ")");
+
+// var time = 0;
+
+// // Scales
+// var x = d3.scaleLog()
+//     .base(10)
+//     .range([0, width])
+//     .domain([142, 150000]);
+// var y = d3.scaleLinear()
+//     .range([height, 0])
+//     .domain([0, 90]);
+// var area = d3.scaleLinear()
+//     .range([25*Math.PI, 1500*Math.PI])
+//     .domain([2000, 1400000000]);
+// var continentColor = d3.scaleOrdinal(d3.schemePastel1);
+
+// // Labels
+// var xLabel = g.append("text")
+//     .attr("y", height + 50)
+//     .attr("x", width / 2)
+//     .attr("font-size", "20px")
+//     .attr("text-anchor", "middle")
+//     .text("GDP Per Capita ($)");
+// var yLabel = g.append("text")
+//     .attr("transform", "rotate(-90)")
+//     .attr("y", -40)
+//     .attr("x", -170)
+//     .attr("font-size", "20px")
+//     .attr("text-anchor", "middle")
+//     .text("Life Expectancy (Years)")
+// var timeLabel = g.append("text")
+//     .attr("y", height -10)
+//     .attr("x", width - 40)
+//     .attr("font-size", "40px")
+//     .attr("opacity", "0.4")
+//     .attr("text-anchor", "middle")
+//     .text("Jan");
+
+// // X Axis
+// var xAxisCall = d3.axisBottom(x)
+//     .tickValues([400, 4000, 40000])
+//     .tickFormat(d3.format("$"));
+// g.append("g")
+//     .attr("class", "x axis")
+//     .attr("transform", "translate(0," + height +")")
+//     .call(xAxisCall);
+
+// // Y Axis
+// var yAxisCall = d3.axisLeft(y)
+//     .tickFormat(function(d){ return +d; });
+// g.append("g")
+//     .attr("class", "y axis")
+//     .call(yAxisCall);
+
+// d3.json("data/calls.json").then(function(data){
+
+//     // Clean data
+//     const formattedData = data.map(function(date){
+//         return date[''].filter(function(team){
+//             var dataExists = (team.units_sold && team.date);
+//             return dataExists
+//         }).map(function(team){
+//             team.units_sold = +team.units_sold;
+//             team.date = +team.date;
+//             return team;            
+//         })
+//     });
+
+//     // Run the code every 0.1 second
+//     d3.interval(function(){
+//         // At the end of our data, loop back
+//         time = (time < 214) ? time+1 : 0
+//         update(formattedData[time]);            
+//     }, 100);
+
+//     // First run of the visualization
+//     update(formattedData[0]);
+
+// })
+
+// function update(data) {
+//     // Standard transition time for the visualization
+//     var t = d3.transition()
+//         .duration(100);
+
+//     // JOIN new data with old elements.
+//     var circles = g.selectAll("circle").data(data, function(d){
+//         return d.team;
+//     });
+
+//     // EXIT old elements not present in new data.
+//     circles.exit()
+//         .attr("class", "exit")
+//         .remove();
+
+//     // ENTER new elements present in new data.
+//     circles.enter()
+//         .append("circle")
+//         .attr("class", "enter")
+//         .attr("fill", function(d) { return teamColor(d.team); })
+//         .merge(circles)
+//         .transition(t)
+//             .attr("cy", function(d){ return y(d.date); })
+//             .attr("cx", function(d){ return x(d.units_sold) })
+//             .attr("r", function(d){ return Math.sqrt(area(d.call_revenue) / Math.PI) });
+
+//     // Update the time label
+//     timeLabel.text(+(time + 1800))
+// }
